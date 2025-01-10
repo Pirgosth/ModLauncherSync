1e8516739c29533cb22f31043a4036bd71204891
PierreDedrieInetum <pierre.dedrie@inetum.com>
2025-01-10 19:18:31 +0100

    feat: exe file is now compressed

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 import py2exe
-    
+
 setup(console=['main.py'], data_files=[
-    ("", [".env"])
-])
+    ("", [".env"]),
+], options = {'py2exe': {'bundle_files': 1, 'optimize': 2, 'compressed': True}})
